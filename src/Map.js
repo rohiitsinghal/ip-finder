368a49d9e5c4439b8c6c817bf2e2b395b26e7f73
ROHIT SINGHAL <singhals.rohit1@gmail.com>
2025-03-23 11:38:55 +0530

    updates

--- src/Map.js
+++ src/Map.js
@@ -1,44 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import Map from 'react-map-gl/mapbox';
-import { Marker } from 'react-map-gl';
-import { RiUserLocationFill } from 'react-icons/ri';
-
-const API_KEY = 'pk.eyJ1Ijoicm9oaXRjb29sIiwiYSI6ImNtOGVyYnAzdDA1NmIycXNkdnVyejUyMW0ifQ.z4RBwSmvvplbC8jUX3VMeg';
-
-const MyMap = ({ lat, lon }) => {
-    const [viewport, setViewport] = useState({
-        latitude: lat,
-        longitude: lon,
-        zoom: 14,
-        bearing: 0,
-        pitch: 0
-    });
-
-    useEffect(() => {
-        setViewport(prev => ({
-            ...prev,
-            latitude: lat,
-            longitude: lon
-        }));
-    }, [lat, lon]);
-
-    return (
-        <div className="map">
-            <Map
-                mapboxAccessToken={API_KEY}
-                initialViewState={viewport}
-                onMove={evt => setViewport(evt.viewState)}
-                style={{ width: "100%", height: "100%" }}
-                mapStyle="mapbox://styles/mapbox/streets-v11"
-            >
-                <Marker latitude={lat} longitude={lon}>
-                    <div className="mark">
-                        <RiUserLocationFill size="25px" color="blue" />
-                    </div>
-                </Marker>
-            </Map>
-        </div>
-    );
-};
-
-export default MyMap;
+import React, { useEffect, useState } from 'react'; 
+import ReactMapGL, { Marker } from 'react-map-gl'; 
+import { RiUserLocationFill } from 'react-icons/ri'; 
+  
+const API_KEY = 'pk.eyJ1Ijoicm9oaXRjb29sIiwiYSI6ImNtOGVyYnAzdDA1NmIycXNkdnVyejUyMW0ifQ.z4RBwSmvvplbC8jUX3VMeg'; 
+  
+const Map = ({ lat, lon }) => { 
+  
+    // Setting up the initial viewport of the map 
+    const [viewport, setViewport] = useState({ 
+        latitude: lat, 
+        longitude: lon, 
+        zoom: 14, 
+        bearing: 0, 
+        pitch: 0, 
+        width: '100%', 
+        height: '100%', 
+    }); 
+  
+    // Viewport re-renders whenever latitude 
+    // and longitude changes 
+    useEffect(() => { 
+        const a = { ...viewport }; 
+        a.latitude = lat; 
+        a.longitude = lon; 
+        setViewport(a); 
+    }, [lat, lon]); 
+  
+    return ( 
+        <div className="map"> 
+            <ReactMapGL 
+                mapboxApiAccessToken={API_KEY} 
+                {...viewport} 
+                onViewportChange={(viewport) => setViewport(viewport)} 
+                mapStyle="mapbox://styles/mapbox/streets-v11"> 
+                <Marker latitude={lat} longitude={lon}> 
+                    <div className="mark"> 
+                        <RiUserLocationFill size="25px" color="blue" /> 
+                    </div> 
+                </Marker> 
+            </ReactMapGL> 
+        </div> 
+    ); 
+}; 
+  
+export default Map;
