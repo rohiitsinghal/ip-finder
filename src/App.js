7370ba1110ffa2f636f746afdbfabd401d1510ef
ROHIT SINGHAL <singhals.rohit1@gmail.com>
2025-03-23 12:26:15 +0530

    updates in dependencies

--- src/App.js
+++ src/App.js
@@ -1,47 +1,57 @@
-import { useEffect, useState } from 'react'; 
-import Axios from 'axios'; 
-import Map from './Map'; 
-import './App.css'; 
-  
-function App() { 
-  
-    // Setting up the initial state variables 
-    const [ipDetails, setIpDetails] = useState([]); 
-    const [lat, setLat] = useState(22.5726); 
-    const [lon, setLon] = useState(88.3832); 
-  
-    // Fetching the API once when the 
-    // component is mounted 
-    useEffect(() => { 
-        Axios.get('https://ipapi.co/json/').then((res) => { 
-            setIpDetails(res.data); 
-            setLat(res.data.latitude); 
-            setLon(res.data.longitude); 
-        }); 
-    }, []) 
-  
-    return ( 
-        <> 
-            <h1 className="heading">IP Address Finder</h1> 
-            <div className="App"> 
-                <div className="left"> 
-                    <h4>What is my IPv4 address?</h4> 
-                    <h1 id="ip">{ipDetails.ip}</h1> 
-                    <h4>Approximate location: </h4> 
-  
-                    <p>{ipDetails.city}, {ipDetails.region}, 
-                        {ipDetails.country_name}.</p> 
-  
-  
-                    <h4>Internet Service Provider(ISP):</h4> 
-  
-                    <p>{ipDetails.org}</p> 
-  
-                </div> 
-                <Map lat={lat} lon={lon} /> 
-            </div> 
-        </> 
-    ); 
-} 
-  
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+
+import Map from "react-map-gl";
+import './App.css';
+
+function App() {
+
+  // Setting up the initial state variables 
+  const [ipDetails, setIpDetails] = useState([]);
+  const [lat, setLat] = useState(22.5726);
+  const [lon, setLon] = useState(88.3832);
+
+  // Fetching the API once when the 
+  // component is mounted 
+  useEffect(() => {
+    Axios.get('https://ipapi.co/json/').then((res) => {
+      setIpDetails(res.data);
+      setLat(res.data.latitude);
+      setLon(res.data.longitude);
+    });
+  }, [])
+
+  return (
+    <>
+      <h1 className="heading">IP Address Finder</h1>
+      <div className="App">
+        <div className="left">
+          <h4>What is my IPv4 address?</h4>
+          <h1 id="ip">{ipDetails.ip}</h1>
+          <h4>Approximate location: </h4>
+
+          <p>{ipDetails.city}, {ipDetails.region},
+            {ipDetails.country_name}.</p>
+
+
+          <h4>Internet Service Provider(ISP):</h4>
+
+          <p>{ipDetails.org}</p>
+
+        </div>
+        <Map
+          initialViewState={{
+            longitude: -74.006,
+            latitude: 40.7128,
+            zoom: 10,
+          }}
+          style={{ width: "100vw", height: "100vh" }}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          mapboxAccessToken="pk.eyJ1Ijoicm9oaXRjb29sIiwiYSI6ImNtOGVyYnAzdDA1NmIycXNkdnVyejUyMW0ifQ.z4RBwSmvvplbC8jUX3VMeg">
+          </Map>
+      </div>
+    </>
+  );
+}
+
 export default App;
